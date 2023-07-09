95cdf7c2a8b024c06f7853146fea5e29955a59e2
Jaolmos <jose.a.olmosmartinez@gmail.com>
2023-07-09 22:56:27 +0200

    parte user realizada

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -9,3 +9,29 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         fields = ['id', 'email', 'username', 'password']
 
 
+    # encriptamos el password de usuario
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+
+        # Validamos que el password no venga vacio
+        if password is not None:
+            instance.set_password(password)
+        
+        instance.save()
+        return instance
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """ Serializador para obtener los datos del usuario """
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'first_name', 'last_name']
+
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    """ Serializador para actualizar los datos del usuario """
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name']
--- users/api/views.py
+++ users/api/views.py
@@ -1,8 +1,10 @@
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from users.api.serializers import UserRegisterSerializer, UserSerializer, UserUpdateSerializer
 from users.models import User
-from users.api.serializers import UserRegisterSerializer
+
 
 class RegisterView(APIView):
     """Vista para registrar Usuarios"""
@@ -14,3 +16,27 @@ class RegisterView(APIView):
             return Response(serializer.data)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+class UserView(APIView):
+    """Vista para obtener los datos del usuario"""
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+    
+    # Metodo para actualizar usuario
+    def put(self, request):
+        user = User.objects.get(id=request.user.id)
+        serializer = UserUpdateSerializer(user, request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+
+
+        
