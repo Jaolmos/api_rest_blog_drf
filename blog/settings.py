f2cd63a336fe79961724224170289c8937641e46
Jaolmos <jose.a.olmosmartinez@gmail.com>
2023-07-09 15:43:34 +0200

    actualizacion

--- blog/settings.py
+++ blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # third-apps
     'rest_framework',
+    'drf_yasg',
     # local apps
     'users',
 ]
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ users/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from users.models import User 
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    """ Serializador para registrar usuarios """
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'password']
+
+
